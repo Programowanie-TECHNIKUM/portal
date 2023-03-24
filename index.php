0f9655323ebf23c841ebe86853458569b36749b0
Szymon Hunek <68203358+Szymon3eK@users.noreply.github.com>
2023-03-24 13:02:12 +0100

    - add files

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel = "stylesheet" href = "style.css">
+</head>
+<body>
+    <div id = "kontener">
+        <h1 style = "font-size: 100px">⇣</h1>
+        <a href = "loguj.php"><button>ZALOGUJ SIE!</button></a>
+    </div>
+</body>
+</html>
+
+<?php
+
+
+
+?>
